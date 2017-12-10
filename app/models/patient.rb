1c5ebeb6f6e236292f85cb8d39d7edde69a53489
Irisvankuijk <32243203+Irisvankuijk@users.noreply.github.com>
2017-12-10 12:16:44 +0100

    Added form validations, general improvements

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,7 @@
 class Patient < ApplicationRecord
+	validates :name, :last_name, :date_birth, :gender, :main_condition, :location, presence: true
 	def age
+		return 0 if date_birth.nil?
 	    age = Date.today.year - date_birth.year
     	age -= 1 if Date.today < date_birth + age.years
     	return age
